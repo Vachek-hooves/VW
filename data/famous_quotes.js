d2e35d52d25622cafb68c0b5a5631fbb4d448882
Vachek-hooves <vachek0602@gmail.com>
2024-07-14 17:43:31 +0300

    create data for quiz

---
+++ data/famous_quotes.js
@@ -0,0 +1,81 @@
+export const QUOTES = [
+  {
+    isLocked: true,
+    id: 'f1',
+    theme: 'Famous People Quotes',
+    questions: [
+      {
+        id: 'fq1',
+        author: 'Johann Wolfgang von Goethe',
+        option:
+          'Welden am Werthersee is a city where time slows down and the soul finds rest.',
+        image: require('../assets/img/famous_people/Johann_Wolfgang_von_Goethe.png'),
+      },
+      {
+        id: 'fq2',
+        author: 'Friedrich Schiller',
+        option:
+          'In Velden am Wörthersee, every street, every house has its own story, which tells about the peace and harmony of this city',
+        image: require('../assets/img/famous_people/Friedrich_Schiller.png'),
+      },
+      {
+        id: 'fq3',
+        author: 'Albert Einstein',
+        option:
+          'The city of Velden am Wörtersee is a place where nature and architecture combine, creating a unique atmosphere of peace and relaxation.',
+        image: require('../assets/img/famous_people/Albert_Einstein.png'),
+      },
+      {
+        id: 'fq4',
+        author: 'Friedrich Nietzsche',
+        option:
+          'In Velden am Wörtersee, every corner is filled with silence, which saves you from the hustle and bustle of the world.',
+        image: require('../assets/img/famous_people/Friedrich_Nietzsche.png'),
+      },
+      {
+        id: 'fq5',
+        author: 'Ralph Waldo Emerson',
+        option:
+          'The city of Velden am Wörtersee is a place where the heart feels calm and thoughts fly freely into infinity.',
+        image: require('../assets/img/famous_people/Ralph_Waldo_Emerson.png'),
+      },
+      {
+        id: 'fq6',
+        author: 'Leonardo da Vinci',
+        option:
+          'In Velden am Werthersee, every stone, every flower, every breath of nature is full of peace and harmony.',
+        image: require('../assets/img/famous_people/Leonardo_da_Vinci.png'),
+      },
+      {
+        id: 'fq7',
+        author: 'Albert Schweitzer',
+        option:
+          'The city of Velden am Wörthersee is a place where time stands still and the heart finds inner peace and talks with nature.',
+        image: require('../assets/img/famous_people/Albert_Schweitzer.png'),
+      },
+      {
+        id: 'fq8',
+        author: 'Carl Jung',
+        option:
+          'In Velden am Wörthersee, every ray of sunshine inspires a new day of peace and relaxation.',
+        image: require('../assets/img/famous_people/Carl_Jung.png'),
+      },
+      {
+        id: 'fq9',
+        author: 'Friedrich Hegel',
+        option:
+          'The city of Velden am Wörthersee is a place where the soul feels harmony with the world around it and finds inner peace.',
+        image: require('../assets/img/famous_people/Friedrich_Hegel.png'),
+      },
+      {
+        id: 'fq10',
+        author: 'Albert Schweitzer',
+        option:
+          'In Velden am Werthersee, every whisper of the wind, every splash of the waves fills the heart with peace and harmony.',
+        image: require('../assets/img/famous_people/Albert_Schweitzer.png'),
+      },
+    ],
+    misticStory:
+      "In the heart of Europe, surrounded by mountain peaks and green forests, is the city of Velden am Wörter See. It was once a picturesque town with a rich history and rich culture. However, one day a dark shadow of a curse fell on the city.The legend said that once upon a time the people of Velden committed a terrible sin that provoked a spiritual curse. Since that time, a sad and invisible darkness covered the city, which brought only misfortune and trouble to its inhabitants.A mysterious curse was associated with an ancient palace that stood on the outskirts of the city. Legend said that it was there that a tragedy took place that brought a curse on the city. Many did not dare even approach this place because of its ominous reputation. However, one day a young explorer named Felix arrived in Velden am Wörter See. He was known for his courage and stubbornness, and decided to solve the mystery of the curse that lay on the city.Felix did his research, looking for ancient texts and legends about curses. He also talked to the townspeople, who gave him their own versions of the events that triggered the curse.Finally, Felix reached an ancient palace on the outskirts of the city. There he found a secret crypt containing ancient scrolls detailing the events that triggered the curse. Felix unfolded the scrolls and revealed the truth about the city's past.It turned out that the curse was the result of lust and betrayal that occurred among the Veldens. Felix understood that in order to lift the curse, a great sacrifice of love and understanding among the inhabitants must be made.Felix began to encourage people to reconcile and work together for the well-being of the city. Gradually, the dark shadow of the curse began to dissipate, and the sky above Velden am Wörter See became clear and clean.The inhabitants of the city began to smile and enjoy life again, and Velden am Wörter See rose from the ashes of the curse, becoming a symbol of the interweaving of the past and the future.",
+  },
+];
